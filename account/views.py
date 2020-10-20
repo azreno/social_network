e3d159fa7742993f48fad51ff3366051e6cda4cb
Anastasiia Ponizovkina <anastasia-pon@mail.ru>
2020-10-21 01:59:01 +0300

    Add login page

--- account/views.py
+++ account/views.py
@@ -9,7 +9,7 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
+            user = authenticate(username=cd['username'], password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
@@ -17,7 +17,7 @@ def user_login(request):
                 else:
                     return HttpResponse('Неактивная учетная запись')
             else:
-                return HttpResponse('Неверный логин')
+                return HttpResponse('Неверные данные')
     else:
         form = LoginForm()
-    return render(request, 'account/login.html', {form: 'form'})
+    return render(request, 'account/login.html', {'form': form})
